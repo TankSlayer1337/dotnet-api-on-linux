2ba3b00b20ababb19458c13e83fed55ba468d1c7
André Säll <erikandresall@gmail.com>
2023-11-20 13:07:34 +0100

    Setup EC2 server stack with CDK.

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -1,16 +1,57 @@
 import * as cdk from 'aws-cdk-lib';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { readFileSync } from 'fs';
 
 export class InfraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const vpc = ec2.Vpc.fromLookup(this, 'DefaultVPC', {
+      isDefault: true
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'InfraQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const securityGroup = new ec2.SecurityGroup(this, 'SecurityGroup', {
+      vpc: vpc,
+      description: 'Allow inbound on port 80 and 443, and all outbound.',
+      allowAllOutbound: true
+    });
+    securityGroup.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(80),
+      'Allow HTTP traffic from anywhere'
+    );
+    securityGroup.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(443),
+      'Allow HTTPS traffic from anywhere'
+    );
+    // SSH ingress rule required for EC2 Instance Connect
+    securityGroup.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(22),
+      'Allow SSH traffic from anywhere'
+    );
+
+    const userDataScript = readFileSync('./lib/user-data.sh', 'utf-8');
+    const userData = ec2.UserData.custom(userDataScript);
+    const ec2Instance = new ec2.Instance(this, 'EC2Instance', {
+      vpc: vpc,
+      vpcSubnets: {
+        subnetType: ec2.SubnetType.PUBLIC
+      },
+      securityGroup: securityGroup,
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T3, ec2.InstanceSize.MICRO),
+      machineImage: ec2.MachineImage.latestAmazonLinux2023(),
+      userDataCausesReplacement: true,
+      userData: userData
+    });
+
+    const elasticIp = new ec2.CfnEIP(this, 'ElasticIP');
+    const elasticIpAssociation = new ec2.CfnEIPAssociation(this, 'ElasticIPAssociation', {
+      allocationId: elasticIp.attrAllocationId,
+      instanceId: ec2Instance.instanceId
+    });
   }
 }
+
--- infra/lib/user-data.sh
+++ infra/lib/user-data.sh
@@ -3,6 +3,7 @@ yum install git -y
 yum install dotnet-sdk-6.0 -y
 git clone https://github.com/TankSlayer1337/dotnet-api-on-linux.git
 cd dotnet-api-on-linux/ExampleWebAPI/ExampleWebAPI
+# dotnet publish will fail if DOTNET_CLI_HOME is not set.
 export DOTNET_CLI_HOME=/tmp
 dotnet publish --configuration Release
 mkdir /var/www
